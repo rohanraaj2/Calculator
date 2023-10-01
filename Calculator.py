c894b5599333f1d875308a754ee7a425a76f61ba
Rohan Raj <rohan.raaj2@gmail.com>
2023-10-02 02:51:03 +0500

    mostly done

--- Calculator.py
+++ Calculator.py
@@ -10,24 +10,65 @@ def multiply (x, y):
 def divide (x, y):
   return x / y
 
-print ("Input first number")
-num1 = int(input())
-print ("Input second number")
-num2 = int(input())
-
-print ("Choose operator:")
-print ("1: Add")
-print ("2: Subract")
-print ("3: Multiply")
-print ("4: Divide")
-
-operation = int(input())
-if operation == 1:
-  add (num1, num2)
-if operation == 2:
-  subract (num1, num2)
-if operation == 3:
-  multiply (num1, num2)
-if operation == 4:
-  divide (num1, num2)
+def operator_selection():
+  print ("Choose operator:")
+  print ("1: Add")
+  print ("2: Subract")
+  print ("3: Multiply")
+  print ("4: Divide")
+  operator = int(input())
+  if operator != 1 and operator != 2 and operator != 3 and operator != 4:
+    print ("Invalid operation. Try again")
+    operator_selection()
+  else:
+    return operator
   
+def num_input():
+  number = input()
+  if number.isdigit() == False:
+    print ("Invalid number. Insert digits only")
+  else:
+    return int(number)
+
+def calculation():
+
+  print ("Input first number:")
+  num1 = num_input()
+  print ("Input second number:")
+  num2 = num_input()
+
+  operator = operator_selection()
+  print ("Your result is", end = " ")
+  if operator == 1:
+    result = add (num1, num2)
+    print (result)
+  elif operator == 2:
+    result = subract (num1, num2)
+    print (result)
+
+  elif operator == 3:
+    result = multiply (num1, num2)
+    print (result)
+
+  elif operator == 4:
+    result = divide (num1, num2)
+    print (result)
+
+  print ("Perform another calculation?")
+  print ("Y: Yes")
+  print ("N: No")
+
+  decision = input()
+
+  if decision == 'Y' or 'y':
+    calculation()
+  elif decision == 'N' or 'n':
+    print ("Thank you for using this calculator")  
+  else:
+    print ("Invalid answer")
+
+calculation()
+
+
+
+
